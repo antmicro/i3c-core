3345c6541826ae7d58a23df189a90548eaa77c74
Mikołaj Szałkowski <mszalkowski@antmicro.com>
2026-05-13 16:06:48 +0200

    test_bus_idle: exotic timings test

--- verification/cocotb/top/lib_i3c_top/test_bus_idle.py
+++ verification/cocotb/top/lib_i3c_top/test_bus_idle.py
@@ -2,16 +2,17 @@
 
 import logging
 import cocotb
-from cocotb.triggers import ClockCycles, Timer
+from cocotb.triggers import ClockCycles, FallingEdge, Timer
 from boot import boot_init
 from i3c_controller_fixed import I3cControllerFixed as I3cController
 from interface import I3CTopTestInterface
 from common import log_seed
+from bus2csr import int2dword
 
 
-_BUS_IDLE_PATH = (
+_BUS_TIMERS_PATH = (
     "xi3c_wrapper.i3c.xcontroller.xcontroller_standby"
-    ".xcontroller_standby_i3c.xbus_timers.bus_idle_o"
+    ".xcontroller_standby_i3c.xbus_timers"
 )
 
 
@@ -41,7 +42,7 @@ async def test_bus_idle(dut):
     Ensures target enters and leaves bus idle state after certain delays.
     """
     i3c_controller, tb = await test_setup(dut)
-    bus_idle_sig = getattr(dut, _BUS_IDLE_PATH)
+    bus_idle_sig = getattr(dut, _BUS_TIMERS_PATH + ".bus_idle_o")
 
     # 1. Generate a manual STOP condition to start the bus timers
     # (The timer requires a STOP detection edge to restart its internal counters)
@@ -62,5 +63,43 @@ async def test_bus_idle(dut):
     i3c_controller.sda = 0
     await Timer(2, "us")
     assert bus_idle_sig.value == 0, "Target should not be in bus idle state"
+
+    await tb.teardown()
+
+
+@cocotb.test
+async def test_exotic_idle_timings(dut):
+    """
+    The bus conditions in the bus_timers module operate independently from each other
+    with each one configured by its own CSR. This introduces the unlikely possibility
+    of T_AVAL < T_FREE and T_IDLE < T_AVAL. This test exists to cover these conditions.
+    """
+
+    i3c_controller, tb = await test_setup(dut)
+    T_FREE = tb.reg_map.I3C_EC.SOCMGMTIF.T_FREE_REG.base_addr
+    T_AVAL = tb.reg_map.I3C_EC.SOCMGMTIF.T_AVAL_REG.base_addr
+    T_IDLE = tb.reg_map.I3C_EC.SOCMGMTIF.T_IDLE_REG.base_addr
+    SIG_FREE = getattr(dut, _BUS_TIMERS_PATH + ".bus_free_o")
+    SIG_AVAL = getattr(dut, _BUS_TIMERS_PATH + ".bus_available_o")
+    SIG_IDLE = getattr(dut, _BUS_TIMERS_PATH + ".bus_idle_o")
+    SIG_BUSY = getattr(dut, _BUS_TIMERS_PATH + ".bus_busy_o")
+
+    # COND 1: BUS AVAILABLE, BUT NOT FREE NOR IDLE
+    await tb.write_csr(T_AVAL, int2dword(0x100), 4)
+    await tb.write_csr(T_FREE, int2dword(0x1000), 4)
+    await tb.write_csr(T_IDLE, int2dword(0x1000), 4)
+    await i3c_controller.send_start()
+    await i3c_controller.send_stop()
+    await FallingEdge(SIG_BUSY)
+    assert ~SIG_FREE.value and ~SIG_IDLE.value and SIG_AVAL.value
+
+    # COND 2: BUS IDLE, BUT NOT AVAILABLE NOR FREE
+    await tb.write_csr(T_AVAL, int2dword(0x1000), 4)
+    await tb.write_csr(T_FREE, int2dword(0x1000), 4)
+    await tb.write_csr(T_IDLE, int2dword(0x100), 4)
+    await i3c_controller.send_start()
+    await i3c_controller.send_stop()
+    await FallingEdge(SIG_BUSY)
+    assert ~SIG_FREE.value and SIG_IDLE.value and ~SIG_AVAL.value
 
     await tb.teardown()
